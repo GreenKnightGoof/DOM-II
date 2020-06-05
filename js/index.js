2e52b34b76bea73560d80f5768e9d74ea13bc600
GreenKnightGoof <60906118+GreenKnightGoof@users.noreply.github.com>
2020-06-05 12:41:51 -0500

    Successfully implemented ten unique events

--- js/index.js
+++ js/index.js
@@ -15,20 +15,73 @@ funBusIMG.addEventListener("mouseleave", () => {
 // MOUSE LEAVE EVENT ABOVE
 
 let buttonFun = document.querySelectorAll(".btn").forEach((button) => {
-    button.addEventListener('dblclick', () => {
-        button.style.transform = "scale(1.2)";
-        button.style.transition = "transform 2s";
-    });
+  button.addEventListener("dblclick", () => {
+    button.style.transform = "scale(1.2)";
+    button.style.transition = "transform 2s";
+  });
 });
 
 // DOUBLE CLICK EVENT ABOVE
 
-window.addEventListener('keydown', checkKeyPress, false);
+window.addEventListener("keydown", checkKeyPress, false);
 
-function checkKeyPress(key){
-    if (key.keyCode == '65'){
-        alert(`OMG SUCH EVENT WOW`)
-    };
-};
+function checkKeyPress(key) {
+  if (key.keyCode == "65") {
+    alert(`OMG SUCH EVENT WOW`);
+  }
+}
 
 // KEYDOWN EVENT ABOVE
+
+let cardFlips = document.querySelectorAll(".destination").forEach((card) => {
+  card.addEventListener("click", () => {
+    event.target.style.transform = "rotate(360deg)";
+    event.target.style.transition = "transform 2s";
+  });
+});
+
+// CLICK EVENT ABOVE
+
+let wheelEvent = document.querySelector(".logo-heading");
+
+wheelEvent.addEventListener("wheel", () => {
+  wheelEvent.style.transform = "scale(.5)";
+  wheelEvent.style.transition = "transform 2s";
+});
+
+// WHEEL EVENT ABOVE
+
+let mouseUp = document.querySelector(".img-fluid.rounded");
+
+mouseUp.addEventListener("mouseup", function () {
+  mouseUp.style.transform = "scale(1.5)";
+  mouseUp.style.transition = "transform 2s";
+});
+
+// MOUSE UP EVENT ABOVE
+
+let mouseMove = document.querySelector(".footer p");
+
+mouseMove.addEventListener("mousemove", () => {
+  mouseMove.style.transform = "scale(2.5)";
+  mouseMove.style.transition = "transform 2s";
+});
+
+// MOUSE MOVE EVENT ABOVE
+
+let mouseOut = document.querySelector(".footer p");
+
+mouseOut.addEventListener("mouseout", () => {
+  mouseOut.style.transform = "scale(1)";
+  mouseOut.style.transition = "transform 2s";
+});
+
+// MOUSE OUT EVENT ABOVE
+
+let bodyEvent = document.querySelector("body");
+
+bodyEvent.addEventListener("contextmenu", () => {
+  bodyEvent.style.backgroundColor = "aquamarine";
+});
+
+// CONTEXT MENU "RIGHT CLICK" EVENT ABOVE
