c2296505a4001699d6f065a671f2c886e2e3e8ba
GreenKnightGoof <60906118+GreenKnightGoof@users.noreply.github.com>
2020-06-05 12:05:31 -0500

    Added first four events

--- js/index.js
+++ js/index.js
@@ -1,1 +1,34 @@
-// Your code goes here
+let funBusIMG = document.querySelector(".intro img");
+
+funBusIMG.addEventListener("mouseenter", () => {
+  funBusIMG.style.transform = "scale(2)";
+  funBusIMG.style.transition = "transform 2s";
+});
+
+// MOUSE ENTER EVENT ABOVE
+
+funBusIMG.addEventListener("mouseleave", () => {
+  funBusIMG.style.transform = "scale(1)";
+  funBusIMG.style.transform = "transform 2s";
+});
+
+// MOUSE LEAVE EVENT ABOVE
+
+let buttonFun = document.querySelectorAll(".btn").forEach((button) => {
+    button.addEventListener('dblclick', () => {
+        button.style.transform = "scale(1.2)";
+        button.style.transition = "transform 2s";
+    });
+});
+
+// DOUBLE CLICK EVENT ABOVE
+
+window.addEventListener('keydown', checkKeyPress, false);
+
+function checkKeyPress(key){
+    if (key.keyCode == '65'){
+        alert(`OMG SUCH EVENT WOW`)
+    };
+};
+
+// KEYDOWN EVENT ABOVE
